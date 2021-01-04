f277056720986fd7f1d2dd362d3fd6e32d684e56
soepono <mr.supono@gmail.com>
2021-01-04 11:29:34 +0700

    New Push

---
+++ application/config/tamaconfig.php
@@ -0,0 +1,5 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['sitename'] = '<i class="fa fa-laptop-code"></i> TamaCIG';
+$config['sitename_mini'] = '<i class="fa fa-laptop-code"></i>';
---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->layout->auth();
+	}
+	
+	public function index()
+	{
+		$data['title'] = 'Dashboard';
+		$data['subtitle'] = '';
+        $data['crumb'] = [
+            'Dashboard' => '',
+        ];
+        //$this->layout->set_privilege(1);
+        $data['page'] = 'Dashboard/Index';
+		$this->load->view('template/backend', $data);
+	}
+
+}
