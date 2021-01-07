59edd63b1aec5ec57e5c3817aa2a00b5bd78d0d0
muhammadakbarid <you@example.com>
2021-01-07 20:21:20 +0700

    Modul Setting Aplikasi

---
+++ application/views/setting/setting_form.php
@@ -0,0 +1,36 @@
+<div class="row">
+    <div class="col-xs-12 col-md-6">
+        <div class="box">
+            <div class="box-header">
+                <h3 class="box-title"><?= $button;?> Setting</h3>
+                <div class="box-tools pull-right">
+                    <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
+                    title="Collapse">
+                    <i class="fa fa-minus"></i></button>
+                     <button type="button" class="btn btn-box-tool" onclick="location.reload()" title="Collapse">
+              <i class="fa fa-refresh"></i></button>
+                </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+        <form action="<?php echo $action; ?>" method="post">
+	    <div class="form-group">
+            <label for="varchar">Kode <?php echo form_error('kode') ?></label>
+            <input type="text" class="form-control" name="kode" id="kode" placeholder="Kode" value="<?php echo $kode; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Nama <?php echo form_error('nama') ?></label>
+            <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama" value="<?php echo $nama; ?>" />
+        </div>
+	    <div class="form-group">
+            <label for="varchar">Nilai <?php echo form_error('nilai') ?></label>
+            <input type="text" class="form-control" name="nilai" id="nilai" placeholder="Nilai" value="<?php echo $nilai; ?>" />
+        </div>
+	    <input type="hidden" name="id" value="<?php echo $id; ?>" /> 
+	    <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
+	    <a href="<?php echo site_url('setting') ?>" class="btn btn-default">Cancel</a>
+	</form>
+         </div>
+        </div>
+    </div>
+</div>
