f4b3dcebd1d55ea069c9120a81b2558d011b3a5d
muhammadakbarid <you@example.com>
2021-01-11 02:20:05 +0700

    database

--- database/akbr_for_u.sql
+++ database/akbr_for_u.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 08, 2021 at 06:59 PM
+-- Generation Time: Jan 10, 2021 at 08:11 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `akbr_for_u`
+-- Database: `mi`
 --
 
 -- --------------------------------------------------------
@@ -40,6 +40,30 @@ CREATE TABLE `akbr_contoh` (
 
 INSERT INTO `akbr_contoh` (`id`, `nama`, `alamat`, `email`) VALUES
 (1, 'Muhammad Akbar', 'Sarijadi, Bandung', 'muhammad.akbar5999@gmail.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `frontend_menu`
+--
+
+CREATE TABLE `frontend_menu` (
+  `id_menu` int(11) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `link` varchar(100) NOT NULL,
+  `id` varchar(100) NOT NULL,
+  `sort` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `frontend_menu`
+--
+
+INSERT INTO `frontend_menu` (`id_menu`, `label`, `link`, `id`, `sort`) VALUES
+(1, 'Home', 'frontend/index', 'Home', 0),
+(2, 'Features', 'frontend/features', 'Features', 1),
+(3, 'About', 'frontend/about', 'about', 2),
+(4, 'Sign in', 'login', 'signin', 3);
 
 -- --------------------------------------------------------
 
@@ -86,9 +110,6 @@ INSERT INTO `groups_menu` (`id_groups`, `id_menu`) VALUES
 (1, 1),
 (3, 1),
 (5, 1),
-(1, 3),
-(3, 3),
-(5, 3),
 (1, 40),
 (1, 92),
 (5, 92),
@@ -108,13 +129,17 @@ INSERT INTO `groups_menu` (`id_groups`, `id_menu`) VALUES
 (5, 105),
 (1, 106),
 (5, 106),
-(1, 108),
 (1, 107),
 (5, 107),
 (1, 4),
 (2, 4),
 (3, 4),
-(5, 4);
+(5, 4),
+(1, 108),
+(1, 109),
+(1, 3),
+(2, 3),
+(3, 3);
 
 -- --------------------------------------------------------
 
@@ -152,12 +177,45 @@ CREATE TABLE `login_attempts` (
   `time` int(11) UNSIGNED DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `login_attempts`
+-- Table structure for table `mahasiswa`
 --
 
-INSERT INTO `login_attempts` (`id`, `ip_address`, `login`, `time`) VALUES
-(7, '::1', 'admin@admin.com', 1610128473);
+CREATE TABLE `mahasiswa` (
+  `id` int(11) NOT NULL,
+  `npm` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `tgl_lahir` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`id`, `npm`, `nama`, `tgl_lahir`) VALUES
+(1, 2193001, 'Alvi Yatul Wardah', '2000-12-09'),
+(2, 2193002, 'Aryaputra Wicaksono', '2001-05-30'),
+(3, 2193003, 'Charles Marpaung', '2000-10-19'),
+(4, 2193004, 'Ester Cibro', '2001-04-07'),
+(5, 2193005, 'Fajar Somantri', '2000-08-10'),
+(6, 2193006, 'Genta Tabah Pengabdian', '2001-04-20'),
+(7, 2193007, 'Gerald Rajagukguk', '2001-06-03'),
+(8, 2193008, 'Grenius Natanael Sidabutar', '2001-12-31'),
+(9, 2193009, 'Ilfah Rifany', '2001-06-02'),
+(10, 2193010, 'Khaliza Diva Qintanada', '2001-09-07'),
+(11, 2193011, 'Koestiyandi Prayoga', '2001-05-17'),
+(12, 2193012, 'Mita Hasanah', '2000-07-19'),
+(13, 2193013, 'Muhammad Akbar', '1999-09-05'),
+(14, 2193014, 'Nazzilla Auliya Putri', '2000-08-11'),
+(15, 2193015, 'Popy Geovani', '2001-04-30'),
+(16, 2193016, 'Prita Fitria Waluyo', '2001-01-09'),
+(17, 2193017, 'Savia Almira Salsabilla', '2001-02-14'),
+(18, 2193018, 'Tegar Nova Silviana', '2001-11-08'),
+(19, 2193019, 'Thifal Irbah Anan', '2000-07-04'),
+(20, 2193020, 'Vinda Ayu Lestari', '2001-01-20'),
+(21, 2193021, 'Zsa Zsa Sabilla', '2001-05-06');
 
 -- --------------------------------------------------------
 
@@ -184,16 +242,17 @@ CREATE TABLE `menu` (
 INSERT INTO `menu` (`id_menu`, `sort`, `level`, `parent_id`, `icon`, `label`, `link`, `id`, `id_menu_type`) VALUES
 (1, 0, 1, 0, 'empty', 'MAIN NAVIGATION', '#', '#', 1),
 (3, 1, 2, 1, 'fas fa-tachometer-alt', 'Dashboard', 'dashboard', '#', 1),
-(4, 8, 2, 40, 'fas fa-table', 'CRUD Generator', 'crudbuilder', '1', 1),
-(8, 6, 2, 40, 'fas fa-bars', 'Menu', 'cms/menu/side-menu', 'navMenu', 1),
+(4, 9, 2, 40, 'fas fa-table', 'CRUD Generator', 'crudbuilder', '1', 1),
+(8, 7, 2, 40, 'fas fa-bars', 'Menu', 'cms/menu/side-menu', 'navMenu', 1),
 (40, 4, 1, 0, 'empty', 'DEV', '#', '#', 1),
-(42, 9, 2, 40, 'fas fa-users-cog', 'User', '#', '1', 1),
-(43, 10, 3, 42, 'fas fa-angle-double-right', 'Users', 'users', '1', 1),
-(44, 11, 3, 42, 'fas fa-angle-double-right', 'Groups', 'groups', '2', 1),
-(89, 7, 2, 40, 'fas fa-th-list', 'Menu Type', 'menu_type', 'menu_type', 1),
+(42, 10, 2, 40, 'fas fa-users-cog', 'User', '#', '1', 1),
+(43, 11, 3, 42, 'fas fa-angle-double-right', 'Users', 'users', '1', 1),
+(44, 12, 3, 42, 'fas fa-angle-double-right', 'Groups', 'groups', '2', 1),
+(89, 8, 2, 40, 'fas fa-th-list', 'Menu Type', 'menu_type', 'menu_type', 1),
 (92, 2, 1, 0, 'empty', 'MASTER DATA', '#', 'masterdata', 1),
 (107, 5, 2, 40, 'fas fa-cog', 'Setting', 'setting', 'setting', 1),
-(108, 3, 2, 92, 'fab fa-affiliatetheme', 'Akbr Contoh', 'akbr_contoh', 'akbar', 1);
+(108, 3, 2, 92, 'fab fa-affiliatetheme', 'Mahasiswa', 'mahasiswa', 'mahasiswa', 1),
+(109, 6, 2, 40, 'fas fa-align-justify', 'Frontend Menu', 'frontend_menu', 'Frontend Menu', 1);
 
 -- --------------------------------------------------------
 
@@ -231,7 +290,7 @@ CREATE TABLE `setting` (
 --
 
 INSERT INTO `setting` (`id`, `kode`, `nama`, `nilai`) VALUES
-(1, 'default.jpg', 'Akbr Template', 'www.muhakbar.com');
+(1, 'default.jpg', 'D3 MI 19', 'www.muhakbar.com');
 
 -- --------------------------------------------------------
 
@@ -265,8 +324,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `ip_address`, `username`, `password`, `salt`, `email`, `activation_code`, `forgotten_password_code`, `forgotten_password_time`, `remember_code`, `created_on`, `last_login`, `active`, `first_name`, `last_name`, `company`, `phone`, `image`) VALUES
-(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@muhakbar.com', '', 'm0vyKu2zW7L8PTG20bquF.707e055aeea8a30aca', 1541329145, 'WcHCQ5vcXwT1z99BvJUWnu', 1268889823, 1610128479, 1, 'Akbar', 'Admin', 'ADMIN', '0', 'akbr_pp_2.jpg'),
-(2, '127.0.0.1', 'member', '$2y$08$ipVAkJ.rjy35wARE9Px47eS2k.gz2FPYy14M019VFwLtBcUax2YJS', '', 'member@member.com', '', 'm0vyKu2zW7L8PTG20bquF.707e055aeea8a30aca', 1541329145, 'lHtbqmxsnla1izZ5LcXd9O', 1268889823, 1546406845, 1, 'Member', 'Apps', 'ADMIN', '0', 'default.jpg');
+(1, '127.0.0.1', 'administrator', '$2a$07$SeBknntpZror9uyftVopmu61qg0ms8Qv1yV6FG.kQOSM.9QhmTo36', '', 'admin@muhakbar.com', '', 'm0vyKu2zW7L8PTG20bquF.707e055aeea8a30aca', 1541329145, 'WcHCQ5vcXwT1z99BvJUWnu', 1268889823, 1610296117, 1, 'Akbar', 'Admin', 'ADMIN', '0', 'akbr_pp_2.jpg'),
+(2, '127.0.0.1', 'member', '$2y$08$ipVAkJ.rjy35wARE9Px47eS2k.gz2FPYy14M019VFwLtBcUax2YJS', '', 'member@member.com', '', 'm0vyKu2zW7L8PTG20bquF.707e055aeea8a30aca', 1541329145, 'lHtbqmxsnla1izZ5LcXd9O', 1268889823, 1610296135, 1, 'Member', 'Apps', 'ADMIN', '0', 'default.jpg');
 
 -- --------------------------------------------------------
 
@@ -299,6 +358,12 @@ ALTER TABLE `akbr_contoh`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `frontend_menu`
+--
+ALTER TABLE `frontend_menu`
+  ADD PRIMARY KEY (`id_menu`);
+
+--
 -- Indexes for table `groups`
 --
 ALTER TABLE `groups`
@@ -316,6 +381,13 @@ ALTER TABLE `list_session_token`
 --
 ALTER TABLE `login_attempts`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `npm` (`npm`) USING BTREE;
 
 --
 -- Indexes for table `menu`
@@ -361,6 +433,12 @@ ALTER TABLE `akbr_contoh`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `frontend_menu`
+--
+ALTER TABLE `frontend_menu`
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
@@ -379,10 +457,16 @@ ALTER TABLE `login_attempts`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
+-- AUTO_INCREMENT for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
 -- AUTO_INCREMENT for table `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=110;
 
 --
 -- AUTO_INCREMENT for table `menu_type`
