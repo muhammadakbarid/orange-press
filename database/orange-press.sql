540debc1b98885076e3e3a0ff45c27b074eaa306
muhammad.akbar5999@gmail.com <muhammad.akbar5999@gmail.com>
2022-04-01 17:45:17 +0700

    update database

--- database/orange-press.sql
+++ database/orange-press.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2022 at 08:16 AM
+-- Generation Time: Apr 01, 2022 at 12:44 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
